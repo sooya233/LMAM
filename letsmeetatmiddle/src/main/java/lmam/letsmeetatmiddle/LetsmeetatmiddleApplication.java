78aa67d56061e92b270920df05c9a34c2826d788
MitchelSong <songminsu2363@gmail.com>
2022-09-23 14:12:06 +0900

    [Feat] : Make Spring Boot Project
    - Make a Spring Boot Project
    - Will add Let's Meet At Middle Backend

---
+++ letsmeetatmiddle/src/main/java/lmam/letsmeetatmiddle/LetsmeetatmiddleApplication.java
@@ -0,0 +1,13 @@
+package lmam.letsmeetatmiddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsmeetatmiddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsmeetatmiddleApplication.class, args);
+	}
+
+}
---
+++ letsmeetatmiddle/src/test/java/lmam/letsmeetatmiddle/LetsmeetatmiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package lmam.letsmeetatmiddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsmeetatmiddleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
